4a782fa6b8eb081d7e8cf78b67b607a362ebc7d9
MOHAMED TISS <m.tiss.2021@alumnos.urjc.es>
2026-08-21 20:58:19 +0200

    style: make unified visual layer load last

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -1,13 +1,8 @@
 (() => {
   'use strict';
   const doc=document;
-  const css=doc.createElement('link');
-  css.rel='stylesheet';
-  css.href='/assets/css/site-pages.css?v=20260821-2';
-  css.dataset.etSitePages='true';
-  doc.head.appendChild(css);
-  const script=doc.createElement('script');
-  script.src='/assets/js/global-core.js?v=20260821-1';
-  script.defer=true;
-  doc.head.appendChild(script);
+  const loadCss=(href,key)=>{if(doc.querySelector(`link[data-${key}]`))return;const link=doc.createElement('link');link.rel='stylesheet';link.href=href;link.dataset[key]='true';doc.head.appendChild(link)};
+  loadCss('/assets/css/site-pages.css?v=20260821-3','etSitePages');
+  loadCss('/assets/css/site-pages-unified.css?v=20260821-1','etUnifiedPages');
+  const script=doc.createElement('script');script.src='/assets/js/global-core.js?v=20260821-2';script.defer=true;doc.head.appendChild(script);
 })();
