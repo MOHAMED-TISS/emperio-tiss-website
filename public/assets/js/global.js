de613f16700797d3edf22399561a7478e1e0ef8f
MOHAMED TISS <m.tiss.2021@alumnos.urjc.es>
2026-08-23 04:15:32 +0200

    chore: cache-bust restored catalogue menu taxonomy

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -15,6 +15,6 @@
   };
   loadCss('/assets/css/site-pages.css?v=20260821-3', 'etSitePages');
   loadCss('/assets/css/site-pages-unified.css?v=20260821-1', 'etUnifiedPages');
-  loadCss('/assets/css/canonical-nav.css?v=20260823-taxonomy-3', 'etCanonicalNav');
-  loadScript('/assets/js/global-core.js?v=20260823-taxonomy-3', 'etGlobalCore');
+  loadCss('/assets/css/canonical-nav.css?v=20260823-catalogue-1', 'etCanonicalNav');
+  loadScript('/assets/js/global-core.js?v=20260823-catalogue-4', 'etGlobalCore');
 })();
