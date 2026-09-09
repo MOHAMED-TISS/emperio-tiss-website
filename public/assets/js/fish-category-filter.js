47567114ae1dc23be3fa32fe018e1ab6b53677a6
MOHAMED TISS <m.tiss.2021@alumnos.urjc.es>
2026-09-09 03:58:48 +0200

    cleanup: remove obsolete fish category state shim

--- public/assets/js/fish-category-filter.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  'use strict';
-
-  // Shared fish catalogue state. fish-catalog.js reads these values when
-  // applying the condition and category filters.
-  window.condition = 'all';
-  window.category = 'all';
-})();
